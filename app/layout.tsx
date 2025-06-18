6d688e3d66ce0225c8b1731103f0556838ef0479
Sahil Sankhyan <shlshrm321@gmail.com>
2025-06-18 13:06:29 +0530

    made changes to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/home/header/Navbar";
-import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
